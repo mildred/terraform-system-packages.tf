40a6c66efd9ab8a496d9bfa24d466a8496aae94c
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2021-11-26 22:35:06 +0100

    Update caddy

--- pkg-caddy.tf/package.tf
+++ pkg-caddy.tf/package.tf
@@ -2,10 +2,6 @@ variable "layer4" {
   default = false
 }
 
-variable "xcaddy_version" {
-  default = "0.1.9"
-}
-
 locals {
   with_l4 = var.layer4 != false
   bin     = local.with_l4 ? "/usr/local/bin/caddy" : "/usr/bin/caddy"
@@ -32,38 +28,12 @@ resource "sys_package" "caddy" {
 # xcaddy
 #
 
-data "sys_os_release" "os_release" {
-}
+resource "sys_file" "caddy_l4" {
+  count    = local.with_l4 ? 1 : 0
+  filename = local.bin
+  source   = "https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fmholt%2Fcaddy-l4"
 
-resource "sys_file" "xcaddy_ar" {
-  count            = local.with_l4 ? 1 : 0
-  target_directory = "/root/xcaddy"
-  source           = "https://github.com/caddyserver/xcaddy/releases/download/v${var.xcaddy_version}/xcaddy_${var.xcaddy_version}_linux_amd64.tar.gz"
-  force_overwrite  = true
-}
-
-resource "sys_file" "xcaddy" {
-  count            = local.with_l4 ? 1 : 0
-  filename         = "/usr/local/bin/xcaddy"
-  source           = "${sys_file.xcaddy_ar[count.index].target_directory}/xcaddy"
   file_permission  = 0755
-}
-
-resource "sys_package" "golang" {
-  count          = local.with_l4 ? 1 : 0
-  type           = "deb"
-  name           = "golang"
-  target_release = data.sys_os_release.os_release.version_codename == "buster" ? "buster-backports" : ""
-}
-
-resource "sys_shell_script" "caddy_l4" {
-  count             = local.with_l4 ? 1 : 0
-  depends_on        = [ sys_package.golang ]
-  working_directory = "/root"
-  filename          = "/usr/local/bin/caddy"
-
-  //create = "${sys_file.xcaddy[count.index].filename} build --with github.com/mholt/caddy-l4 --output /usr/local/bin/caddy"
-  create = "${sys_file.xcaddy[count.index].filename} build --with github.com/mholt/caddy-l4@master --output /usr/local/bin/caddy"
 }
 
 #
@@ -75,7 +45,7 @@ output "bin" {
 }
 
 output "done" {
-  value = [sys_shell_script.caddy_l4.*.id, sys_package.caddy.*.id]
+  value = [sys_file.caddy_l4.*.id, sys_package.caddy.*.id]
 }
 
 
